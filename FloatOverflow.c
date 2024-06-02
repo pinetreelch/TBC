9ff10ceab690097d95e55b89f0a45570aad06820
이찬호 <ichanho@ichanhoui-MacBookPro.local>
2024-06-02 19:52:58 +0900

    홍정모 따베시 lecture 3.7

---
+++ FloatOverflow.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int main()
+{
+  //  unsigned int i = 0b11111111111111111111111111111111; //4byte 0b는 내가 입력한 111111... 이 리터널이 binary즉 이진수다라는것. unsigned int 출력했을때 메모리 4칸을 차지함 => 4UL. 1byte =8 bit 즉 4bype = 8 * 4 = 32
+//    unsigned int u = UINT_MAX;
+
+    //unsigned int u_max = UINT_MAX;
+    // unsigned int u_min = 0;
+    // signed int i_max = INT_MAX;
+    // signed int i_min = INT_MIN;
+
+    
+    // printf("max of uint = %u\n", u_max);
+    // printf("mint of uint = %u\n", u_min);
+    // printf("max of int%d\n", i_max);
+    // printf("min of int%d\n", i_min);
+
+
+    // i to binary representation
+    // char buffer[33];
+    //print decimal and binary
+
+    //sizeof(unsigned int); 
+    //printf("%u", sizeof(unsigned int));
+    //printf("%u", sizeof(i)); 
+    
+    unsigned int u_max = UINT_MAX;
+
+    char buffer[33];
+    _itoa(u_max, buffer, 2);
+
+    printf("decimal: %u\n", u_max);
+    printf("binary: %s\n", buffer);
+
+
+    return 0;
+}
---
+++ Hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //** tasks.json 생성
+    // cmd + shift + B -> C/C++: gcc 활성 파일 필드의 톱니바퀴 클릭 => .vscode 폴더 하위에 tasks.json 파일 생성
+
+    //** launch.json 파일 생성
+    // 소스파일에서 F5 클릭 후 , LLDB 선택 
+    
+    //F5->LLDB를 룰러서 디버깅모드에 진입
+    
+    printf("Hello World \n");
+    return 0;
+}
