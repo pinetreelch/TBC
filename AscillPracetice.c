433fd5cbf05cd9fdabcabc022edc665682cf9225
= <=>
2024-06-03 22:43:31 +0900

    ASCII && escape sequence

---
+++ AscillPracetice.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char c = 'A';
+    char d = '*';    //d = 'A'
+
+    //printf("%c %hhd\n", c, c); // 문자로서도 출력, 정수로서도 출력 가능
+    //printf("%c %hhd\n", d, d);
+
+    //printf("%c \n", c + 1);
+
+    //char a = '\a';  //escape sequence
+    //printf("%c", c); //맥북에서는 소리 안나네
+
+    //printf("\07");
+    //printf("\x23");
+
+    // float salary;
+    // printf("$______\b\b\b\b\b\b");
+    // scanf("%f", &salary);
+
+    // printf("AB\tCDEF\n");
+    // printf("ABC\tDEF\n");
+
+    printf("\\ \ 'HA+\' \"Hello\" \?\n");
+
+    return 0;
+}
